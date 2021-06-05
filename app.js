d96d24077823ad0605b29318670ead818523f21a
wabualela <wailabualela@gmail.com>
2021-06-05 15:24:33 +0200

    progress

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn_signing = document.querySelector(".btn-signing");
+const btn_signup = document.querySelector(".btn-signup");
+const form_fox = document.querySelector(".form_fox");
+
+btn_signing.onclick = function() {
+    form_fox.classList.add("active");
+};
